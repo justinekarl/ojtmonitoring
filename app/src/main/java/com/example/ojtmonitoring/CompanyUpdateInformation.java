edfeb393effdec953ad35509d13685330a333419
justinekarl <justinekarlmontemayor27@gmail.com>
2018-02-19 20:38:41 +0800

    2018-02-19

---
+++ app/src/main/java/com/example/ojtmonitoring/CompanyUpdateInformation.java
@@ -0,0 +1,15 @@
+package com.example.ojtmonitoring;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jomer.filetracker.R;
+
+public class CompanyUpdateInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_update_information);
+    }
+}
---
+++ app/src/main/java/com/example/ojtmonitoring/CreateUpdateResumeActivity.java
@@ -0,0 +1,15 @@
+package com.example.ojtmonitoring;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jomer.filetracker.R;
+
+public class CreateUpdateResumeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_update_resume);
+    }
+}
